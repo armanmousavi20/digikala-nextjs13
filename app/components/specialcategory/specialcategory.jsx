2e26bd864006202a9850836b9c1f815230379166
arman <armman.mousavi20@gmail.com>
2023-05-03 03:53:52 -0700

    commit4-specialcategories

--- app/components/specialcategory/specialcategory.jsx
+++ app/components/specialcategory/specialcategory.jsx
@@ -14,55 +14,55 @@ const SpecialCategory = () =>
     return(<div className='flex justify-between mx-4 my-4'>
                <div>
                <Image src={img9} width={85} height={85} 
-                      className='border-2 border-red-600 p-1 rounded-full'/>
+                      className='border-2 border-red-600 p-1 rounded-full cursor-pointer'/>
                       <p className='mt-1 font-bold'> وسایل کمپ  </p>
                </div>
 
                <div>
                <Image src={img8} width={85} height={85} 
-                      className='border-2 border-red-600 p-1 rounded-full'/>
+                      className='border-2 border-red-600 p-1 rounded-full cursor-pointer'/>
                       <p className='mt-1 font-bold'> قمقمه سفری  </p>
                </div>
 
                <div>
                <Image src={img7} width={85} height={85} 
-                      className='border-2 border-red-600 p-1 rounded-full'/>
+                      className='border-2 border-red-600 p-1 rounded-full cursor-pointer'/>
                       <p className='mt-1 font-bold'> انواع دریل  </p>
                </div>
 
                <div>
                <Image src={img6} width={85} height={85} 
-                      className='border-2 border-red-600 p-1 rounded-full'/>
+                      className='border-2 border-red-600 p-1 rounded-full cursor-pointer'/>
                       <p className='mt-1 font-bold'> کارت بازی   </p>
                </div>
 
                <div>
                <Image src={img5} width={85} height={85} 
-                      className='border-2 border-red-600 p-1 rounded-full'/>
+                      className='border-2 border-red-600 p-1 rounded-full cursor-pointer'/>
                       <p className='mt-1 font-bold'> دکور جدید  </p>
                </div>
 
                <div>
                <Image src={img4} width={85} height={85} 
-                      className='border-2 border-red-600 p-1 rounded-full'/>
+                      className='border-2 border-red-600 p-1 rounded-full cursor-pointer'/>
                       <p className='mt-1 font-bold'> سینما  </p>
                </div>
 
                <div>
                <Image src={img3} width={85} height={85} 
-                      className='border-2 border-red-600 p-1 rounded-full'/>
+                      className='border-2 border-red-600 p-1 rounded-full cursor-pointer'/>
                       <p className='mt-1 font-bold'> پوست جدید  </p>
                </div>
 
                <div>
                <Image src={img2} width={85} height={85} 
-                      className='border-2 border-red-600 p-1 rounded-full'/>
+                      className='border-2 border-red-600 p-1 rounded-full cursor-pointer'/>
                       <p className='mt-1 font-bold'>  گوشی موبایل </p>
                </div>
 
                <div>
                <Image src={img1} width={85} height={85} 
-                      className='border-2 border-red-600 p-1 rounded-full'/>
+                      className='border-2 border-red-600 p-1 rounded-full cursor-pointer'/>
                       <p className='mt-1 font-bold'> خاطره انگیز </p>
                </div>
                  
