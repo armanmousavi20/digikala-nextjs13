b9c4e100969a2eba39b4cc2c103180967f5dcbc5
arman <armman.mousavi20@gmail.com>
2023-05-04 01:53:58 -0700

    commit5-slider

---
+++ app/components/slider/slider.jsx
@@ -0,0 +1,44 @@
+'use client';
+import React from 'react';
+import Image from 'next/image';
+
+import img1 from '../../../public/images/slider/1.gif';
+import img2 from '../../../public/images/slider/2.jpg';
+import img3 from '../../../public/images/slider/3.jpg';
+import img4 from '../../../public/images/slider/4.jpg';
+
+import { Swiper , SwiperSlide } from 'swiper/react';
+import SwiperCore , { Autoplay , Navigation, Pagination} from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
+const Slider = () =>
+{
+     const images = [{title :img1 , id:1 } , 
+                     {title :img2 , id:2 } , 
+                     {title :img3 , id:3 } , 
+                    {title :img4 , id:4 }]
+    SwiperCore.use([Autoplay])
+   // const navigationPrevRef = useRef(null);
+  //  const navigationNextRef = useRef(null);    
+    return(<div className='h-[350px] bg-red-300'>              
+                <Swiper  modules={[Navigation, Pagination]}
+                       spaceBetween={50} slidesPerView={1} navigation 
+                       pagination={{ clickable : true }} loop={true} 
+                       autoplay={{ delay:5000 }} 
+                       onSwiper={(swiper) => console.log(swiper)}
+                       onSlideChange={() => console.log('slide change')}>
+                         {images.map( (img) => {
+                              return(
+                                 <SwiperSlide key={img.id} className='h-full'>               
+                                    <Image src={img.title} width={1500} height={400} className='md:h-[350px]'  />
+                                 </SwiperSlide>
+                              )}
+                              )}
+
+                </Swiper>  
+           </div>)
+}
+export default Slider;
--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
-import Image from 'next/image'
 import SpecialCategory from './components/specialcategory/specialcategory';
+import Slider from './components/slider/slider';
+
 
 export default function Home() {
   return (
           <div>
               <SpecialCategory />
+              <Slider />
           </div>
   );
 }
